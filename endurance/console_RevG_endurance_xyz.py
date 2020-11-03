f9703f290d8207e64e23144ffde8d839caa88373
vital <vital@PNY-P3000-1>
2020-11-03 16:15:22 +0000

    Endurance test for console interface PCB revG

---
+++ endurance/console_RevG_endurance_xyz.py
@@ -0,0 +1,70 @@
+# Turns spindle on and off at an equally spaced mesh of XYZ co-ordinates within a defined volume
+# commit test 1
+
+lines = ['(T1  NO TOOL NEEDED)',
+        'G90', #Absolute
+        'G94', #Feed units per mm
+        'G17', #XY plane
+        'G21' #In MM
+        ]
+
+# lines.append('G1 X0 Y0 Z-1 F3000') #Ugly, but sets feed rate (can we do F on it's own etc?)
+
+# INPUTS: Grid settings 
+x_start = 0
+x_end = 1250
+x_increment = 50
+
+y_start = 0
+y_end = 2400
+y_increment = 200
+
+z_start = 120
+z_end = 0
+z_increment = 20
+
+
+# POPULATE GRID
+x_grid = []
+x = x_start
+while x < x_end:
+    x_grid.append(x)
+    x += x_increment
+x_grid.append(x_end)
+print x_grid
+
+y_grid = [] 
+y = y_start
+while y < y_end:
+    y_grid.append(y)
+    y += y_increment
+y_grid.append(y_end)
+print y_grid    
+
+z_grid = []
+z = z_start
+while z > z_end:
+    z_grid.append(z)
+    z -= z_increment
+z_grid.append(z_end)
+print z_grid    
+
+count = 0 
+for y in y_grid:
+    for z in z_grid:
+        for x in x_grid:
+            lines.append("M3 S25000") #Spindle on
+            lines.append("G4 P4") #Wait (to ensure full speed)            
+            lines.append("G1 X" + str(x) + " Y" + str(y) + " Z" + str(z) + " F3000") #Go to next co-ord
+            lines.append("M5") #Kill spindle
+            lines.append("G4 P4") #Allow spindle to stop 
+            count += 1
+
+lines.append("M30") #Prog end
+lines.append("%") #Prog end (redundant?)
+  
+print count
+f = open("XYZ_spindle_endurance.nc", "w")
+for line in lines:
+    f.write(line + "\n")   
+
