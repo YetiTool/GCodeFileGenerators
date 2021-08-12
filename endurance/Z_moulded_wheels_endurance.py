9ccfdbbc585e18382bfdf02a12a4aceb217ba855
vital <vital@PNY-V>
2021-08-12 11:19:39 +0100

    Z endurance test added

--- endurance/Z_moulded_wheels_endurance.py
+++ endurance/Z_moulded_wheels_endurance.py
@@ -9,20 +9,21 @@ lines = ['(T1 SPINDLE ONLY, NO ROUTER CUTTER)',
         'G94', #Feed units per mm
         'G17', #XY plane
         'G21', #In MM
+        'G0 X0 Y0'
         ]
 
 # CYCLES
 cycles_to_test = 1000
 
 # DEFINE Z GRID
-z_start = -1.000
-z_end = -150.000
+z_start = 1.000
+z_end = 148.000
 
 lines_count = 0
 
 while lines_count < cycles_to_test:
-	lines.append("G53 G0 Z"+str(z_end)) #Go to bottom
-	lines.append("G53 G0 Z"+str(z_start)) #Go to top
+	lines.append("G0 Z"+str(z_end)) #Go to bottom
+	lines.append("G0 Z"+str(z_start)) #Go to top
 	lines_count += 1
   
 lines.append("M30") #Prog end
